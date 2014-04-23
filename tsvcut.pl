83ba6596b25ff1455232e1e2e82e894b5c9e97fd
Jeff Lawson <jeff.lawson@flightaware.com>
2014-04-23 11:21:02 -0500

    add new -t option to output field names too.

--- tsvcut.pl
+++ tsvcut.pl
@@ -2,8 +2,21 @@
 
 use strict;
 
-while (<STDIN>) {
-    chomp;
-    my %tsv = split("\t", $_);
-    print join("\t", @tsv{@ARGV}) . "\n";
+if ($ARGV[0] eq "-t") {
+    # output column identifiers
+    shift(@ARGV);
+    while (<STDIN>) {
+	chomp;
+	my %tsv = split("\t", $_);
+	my @result = map { ($_, $tsv{$_}) if exists($tsv{$_}) } @ARGV;
+	print join("\t", @result) . "\n";
+    }
+
+} else {
+    # no column identifiers
+    while (<STDIN>) {
+	chomp;
+	my %tsv = split("\t", $_);
+	print join("\t", @tsv{@ARGV}) . "\n";
+    }
 }
