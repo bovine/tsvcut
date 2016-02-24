8f2e4f42cece690f4e9c46c7f80d5245e8580b1b
Jeff Lawson <jeff.lawson@flightaware.com>
2016-02-24 19:56:42 +0000

    add a Tcl port with the same functionality, mostly for performance comparison purposes.

---
+++ tsvcut.tcl
@@ -0,0 +1,32 @@
+#!/usr/local/bin/tclsh
+
+if {[lindex $::argv] eq "-t"} {
+	# output column identifiers
+	set ::argv [lrange $::argv 1 end]
+	while {[gets stdin line] >= 0} {
+		array unset tsv
+		array set tsv [split $line "\t"]
+		set result {}
+		foreach term $::argv {
+			if {[info exists $tsv($term)]} {
+				lappend result $term $tsv($term)
+			}
+		}
+		puts [join $result "\t"]
+	}
+} else {
+	# no column identifiers
+	while {[gets stdin line] >= 0} {
+		array unset tsv
+		array set tsv [split $line "\t"]
+		set result {}
+		foreach term $::argv {
+			if {[info exists $tsv($term)]} {
+				lappend result $tsv($term)
+			} else {
+				lappend result ""
+			}
+		}
+		puts [join $result "\t"]
+	}
+}
