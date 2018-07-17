addd30fc016408860ea09e66389b6c6eaed5191d
Jeff Lawson <jeff.lawson@flightaware.com>
2018-07-17 23:52:12 +0000

    new script to print a pretty ASCII table from your TSV

---
+++ tsvformat.tcl
@@ -0,0 +1,71 @@
+#!/usr/local/bin/tclsh
+
+# Prints out a tsv file in a pretty fixed-width table.
+
+set rows {}
+set cols {}
+
+# read the entire input and compute maximum column widths.
+while {[gets stdin line] >= 0} {
+	array unset tsv
+	array set tsv [split $line "\t"]
+
+	foreach {key value} [array get tsv] {
+		
+		if {![info exists width($key)]} {
+			set width($key) [string length $key]
+			lappend cols $key
+		}
+
+		set lenval [string length $value]
+		if {$width($key) < $lenval} {
+			set width($key) $lenval
+		}
+	}
+
+	lappend rows $line
+}
+if {$rows == ""} {
+	exit 0
+}
+
+# print the dotted separator line.
+set separator "+"
+foreach {key size} [array get width] {
+	append separator [string repeat "-" [expr {$size + 2}]]
+	append separator "+"
+}
+puts $separator
+
+# print the header row.
+set result {}
+foreach {key size} [array get width] {
+	lappend result [format "%-*s" $size $key]
+}
+puts "| [join $result { | }] |"
+
+# print the dotted separator line.
+puts $separator
+
+# print the data with proper column widths.
+foreach line $rows {
+	array unset tsv
+	array set tsv [split $line "\t"]
+
+	set result {}
+	foreach {key size} [array get width] {
+		if {[info exists tsv($key)]} {
+			lappend result [format "%-*s" $size $tsv($key)]
+		} else {
+			lappend result [string repeat " " $size]
+		}
+	}
+	puts "| [join $result { | }] |"
+}
+
+# print the dotted separator line.
+puts $separator
+
+# print the row count.
+puts "[llength $rows] rows."
+exit 0
